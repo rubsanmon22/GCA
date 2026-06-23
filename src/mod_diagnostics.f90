ac857843d7be448e7c7c121e241732ac072758c2
rubsanmon <ruben.sandez@unc.edu.ar>
2026-06-23 11:49:55 -0300

    incorporación de nuevas velocidades al calculo de Ekin

--- src/mod_diagnostics.f90
+++ src/mod_diagnostics.f90
@@ -10,17 +10,22 @@ module mod_diagnostics
   use mod_grid
   use mod_particles
   use mod_cic
+  use mod_evolution_guiding_center !se incorpora para calcular la energia cinetica con las velocidades de los drifts
+
   implicit none
 contains
-  subroutine compute_energies(cfg,grid,part,initial)
+  
+subroutine compute_energies(cfg,grid,part,initial)
     type(config_t), intent(in) :: cfg
     type(grid_t), intent(in) :: grid
     type(particles_t), intent(inout) :: part
     logical, intent(in) :: initial
     integer :: ip
     real(dp) :: ekin
+    real(dp) :: dxdt,dydt,dzdt,dvpardt !velocidades que se incorporan a la energia cinetica
     type(field_sample_t) :: fs
     logical :: inside
+
     do ip=1,part%np
        if (.not. part%active(ip)) then
           ekin=-1.0_dp
@@ -29,7 +34,11 @@ contains
           if (.not. inside) then
              ekin=-1.0_dp
           else
-             ekin=0.5_dp*cfg%m_particle*part%vpar(ip)**2 + part%mu(ip)*fs%bmag
+            ! llamo las variables necesarias para el cálculo
+            call guiding_center_rhs(cfg, part%x(ip), part%y(ip), part%z(ip), part%vpar(ip), part%mu(ip), fs, dxdt, dydt, dzdt, dvpardt)
+            ! nuevo valor de ekin
+            ekin=0.5_dp*cfg%m_particle*(dxdt**2 + dydt**2 + dzdt**2) + part%mu(ip)*fs%bmag
+            ! ekin=0.5_dp*cfg%m_particle*part%vpar(ip)**2 + part%mu(ip)*fs%bmag
           end if
        end if
        if (initial) then
